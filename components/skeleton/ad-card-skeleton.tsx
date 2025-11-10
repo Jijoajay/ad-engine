bdd7db5c04dbbdc054348472bead8d746790a8d7
Ajay <jijoajay45@gmail.com>
2025-11-10 18:25:57 +0530

    worked on skeleton

--- components/skeleton/ad-card-skeleton.tsx
+++ components/skeleton/ad-card-skeleton.tsx
@@ -4,36 +4,34 @@ import AdminLayout from "@/layout/AdminLayout";
 
 export const AdCardSkeleton = () => {
   return (
-    <AdminLayout>
-      <div className="bg-[#231F29] w-full flex flex-col sm:flex-row border border-[#4C4C4C] rounded-xl overflow-hidden animate-pulse">
-        <div className="flex flex-col justify-between sm:w-[65%] w-full p-6 gap-6">
-          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
-            <div className="space-y-4 w-full sm:w-auto">
-              <div className="space-y-2">
-                <div className="h-5 w-40 bg-gray-700 rounded"></div>
-                <div className="h-4 w-56 bg-gray-700 rounded"></div>
-              </div>
-              <div className="space-y-2">
-                <div className="h-5 w-36 bg-gray-700 rounded"></div>
-                <div className="h-4 w-64 bg-gray-700 rounded"></div>
-              </div>
-              <div className="space-y-2">
-                <div className="h-5 w-28 bg-gray-700 rounded"></div>
-                <div className="h-4 w-40 bg-gray-700 rounded"></div>
-              </div>
+    <div className="bg-[#231F29] w-full flex flex-col sm:flex-row border border-[#4C4C4C] rounded-xl overflow-hidden animate-pulse">
+      <div className="flex flex-col justify-between sm:w-[65%] w-full p-6 gap-6">
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4">
+          <div className="space-y-4 w-full sm:w-auto">
+            <div className="space-y-2">
+              <div className="h-5 w-40 bg-gray-700 rounded"></div>
+              <div className="h-4 w-56 bg-gray-700 rounded"></div>
             </div>
-            <div className="h-8 w-24 bg-gray-700 rounded"></div>
+            <div className="space-y-2">
+              <div className="h-5 w-36 bg-gray-700 rounded"></div>
+              <div className="h-4 w-64 bg-gray-700 rounded"></div>
+            </div>
+            <div className="space-y-2">
+              <div className="h-5 w-28 bg-gray-700 rounded"></div>
+              <div className="h-4 w-40 bg-gray-700 rounded"></div>
+            </div>
           </div>
-
-          {/* Bottom button placeholder */}
-          <div className="flex justify-start w-full md:justify-center">
-            <div className="h-10 w-full bg-gray-700 rounded"></div>
-          </div>
+          <div className="h-8 w-24 bg-gray-700 rounded"></div>
         </div>
 
-        {/* Right Section (image) */}
-        <div className="relative sm:w-[35%] w-full h-[250px] sm:h-auto bg-gray-800"></div>
+        {/* Bottom button placeholder */}
+        <div className="flex justify-start w-full md:justify-center">
+          <div className="h-10 w-full bg-gray-700 rounded"></div>
+        </div>
       </div>
-    </AdminLayout>
+
+      {/* Right Section (image) */}
+      <div className="relative sm:w-[35%] w-full h-[250px] sm:h-auto bg-gray-800"></div>
+    </div>
   );
 };
